9df75572ba68eed783ba9bab4237c0f8a4f844e7
Pablo <ortegaperaltap@gmail.com>
2025-06-27 22:22:50 -0400

    Add crypto dashboard table to main page
    Introduced a new crypto dashboard table in index.html to display cryptocurrency data. Added related DOM selectors in app.js and provided new styles in styles.css for layout and table formatting.

---
+++ app.js
@@ -0,0 +1,10 @@
+const body = document.querySelector('body')
+const userIcon = document.querySelector('.user')
+const cryptoDashboard = document.querySelector('#cryptoDashboard')
+
+const cryptoInfo = document.querySelectorAll('#cryptoInfo')
+const cryptoNumber = document.querySelectorAll('#cryptoNumber')
+const cryptoName = document.querySelectorAll('#cryptoName')
+const cryptoPrice = document.querySelectorAll('#cryptoPrice')
+const cryptoChange = document.querySelectorAll('#cryptoChange')
+
