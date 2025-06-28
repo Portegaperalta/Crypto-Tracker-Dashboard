cbbbfdb07fbf8825e4813a0e7454736617203a78
Pablo <ortegaperaltap@gmail.com>
2025-06-27 23:35:54 -0400

    Add function to fetch crypto data from API
    Introduced fetchCryptoData, an async function that retrieves cryptocurrency market data from the CoinGecko API using axios and logs the results. Includes error handling and API key in request headers.

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ const cryptoName = document.querySelectorAll('#cryptoName')
 const cryptoPrice = document.querySelectorAll('#cryptoPrice')
 const cryptoChange = document.querySelectorAll('#cryptoChange')
 
+const fetchCryptoData = async () => {
+
+  try {
+    const config = {
+      headers:
+      {
+        Accept: 'application/json',
+        'x-cg-demo-api-key': 'CG-pweCeHuWXX9ni58BjogPQnSh'
+      },
+      params: {
+        vs_currency: 'usd',
+        order: 'market_cap_desc',
+        per_page: 10,
+        page: 1,
+        sparkline: false
+      }
+    }
+
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets/', config)
+    const coins = res.data
+    console.log(coins)
+  }
+  catch (error) {
+    console.log(`Error:${error}`)
+  }
+}
